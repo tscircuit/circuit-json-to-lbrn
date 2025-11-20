df257dea884ff67c0bd7e2b4ef6688a3a5893984
seveibar <seveibar@gmail.com>
2025-11-19 23:50:52 -0800

    bool2

--- lib/index.ts
+++ lib/index.ts
@@ -5,6 +5,8 @@ import type { ConvertContext } from "./ConvertContext"
 import { addPlatedHole } from "./element-handlers/addPlatedHole"
 import { addSmtPad } from "./element-handlers/addSmtPad"
 import { addPcbTrace } from "./element-handlers/addPcbTrace"
+import { getFullConnectivityMapFromCircuitJson } from "circuit-json-to-connectivity-map"
+import { Polygon, Box } from "@flatten-js/core"
 
 export const convertCircuitJsonToLbrn = (
   circuitJson: CircuitJson,
@@ -24,19 +26,27 @@ export const convertCircuitJsonToLbrn = (
   })
   project.children.push(copperCutSetting)
 
+  const connMap = getFullConnectivityMapFromCircuitJson(circuitJson)
+
   const ctx: ConvertContext = {
     db,
     project,
     copperCutSetting,
+    connMap,
+    netGeoms: new Map(),
+  }
+
+  for (const net of Object.keys(connMap.netMap)) {
+    ctx.netGeoms.set(net, new Polygon())
   }
 
   for (const smtpad of db.pcb_smtpad.list()) {
     addSmtPad(smtpad, ctx)
   }
 
-  for (const platedHole of db.pcb_plated_hole.list()) {
-    addPlatedHole(platedHole, ctx)
-  }
+  // for (const platedHole of db.pcb_plated_hole.list()) {
+  //   addPlatedHole(platedHole, ctx)
+  // }
 
   for (const trace of db.pcb_trace.list()) {
     addPcbTrace(trace, ctx)
