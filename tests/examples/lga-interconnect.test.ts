b320910e02e68132a56ac5c93b4b6c419a4dd3de
Anas sarkiz <anassoftwaredev@gmail.com>
2025-12-04 14:32:45 +0200

    Type fix

--- tests/examples/lga-interconnect.test.ts
+++ tests/examples/lga-interconnect.test.ts
@@ -6,11 +6,12 @@ import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 import { generateLightBurnSvg } from "lbrnts"
 import { convertCircuitJsonToLbrn } from "../../lib"
 import { stackSvgsVertically } from "stack-svgs"
+import type { CircuitJson } from "circuit-json"
 
 test("lga-interconnect", async () => {
-  const pcbSvg = await convertCircuitJsonToPcbSvg(circuitJson)
+  const pcbSvg = await convertCircuitJsonToPcbSvg(circuitJson as CircuitJson)
 
-  const project = convertCircuitJsonToLbrn(circuitJson)
+  const project = convertCircuitJsonToLbrn(circuitJson as CircuitJson)
 
   Bun.write("debug-output/lga-interconnect.lbrn2", project.getString(), {
     createPath: true,
