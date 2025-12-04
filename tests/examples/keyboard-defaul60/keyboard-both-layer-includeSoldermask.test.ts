e8aaaa916789dd41e80d92a68738a4fe02b0c6ec
Anas sarkiz <anassoftwaredev@gmail.com>
2025-12-04 14:16:58 +0200

    separate keyboard test files

---
+++ tests/examples/keyboard-defaul60/keyboard-both-layer-includeSoldermask.test.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "bun:test"
+import circuitJson from "../../assets/keyboard-default60.json" with {
+  type: "json",
+}
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import { generateLightBurnSvg } from "lbrnts"
+import { convertCircuitJsonToLbrn } from "../../../lib"
+import { stackSvgsVertically } from "stack-svgs"
+
+test("keyboard-both-layers-includeSoldermask", async () => {
+  const pcbSvg = await convertCircuitJsonToPcbSvg(circuitJson)
+
+  const project = convertCircuitJsonToLbrn(circuitJson, {
+    includeLayers: ["top", "bottom"],
+    includeSoldermask: true,
+  })
+
+  Bun.write("debug-output/keyboard-both-layers.lbrn2", project.getString(), {
+    createPath: true,
+  })
+
+  const lbrnSvg = await generateLightBurnSvg(project)
+
+  expect(stackSvgsVertically([pcbSvg, lbrnSvg])).toMatchSvgSnapshot(
+    import.meta.filename,
+  )
+})
