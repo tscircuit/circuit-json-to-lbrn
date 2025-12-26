d0c134a7a1e67fab353fa804bfaa475254c5897a
seveibar <seveibar@gmail.com>
2025-12-26 09:54:57 -0800

    add example02

---
+++ tests/examples/example02/example02.test.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "bun:test"
+import circuitJson from "./example02.circuit.json" with { type: "json" }
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import { generateLightBurnSvg } from "lbrnts"
+import { convertCircuitJsonToLbrn } from "lib/index"
+import { stackSvgsVertically } from "stack-svgs"
+import type { CircuitJson } from "circuit-json"
+
+test("example02 - two connected test points", async () => {
+  const pcbSvg = await convertCircuitJsonToPcbSvg(circuitJson as CircuitJson)
+
+  const project = convertCircuitJsonToLbrn(circuitJson as CircuitJson)
+
+  // Bun.write("debug-output/lga-interconnect.lbrn2", project.getString(), {
+  //   createPath: true,
+  // })
+
+  const lbrnSvg = await generateLightBurnSvg(project)
+
+  expect(lbrnSvg).toMatchSvgSnapshot(import.meta.filename)
+
+  // expect(stackSvgsVertically([pcbSvg, lbrnSvg])).toMatchSvgSnapshot(
+  //   import.meta.filename,
+  // )
+})
